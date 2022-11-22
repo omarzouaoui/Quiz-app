90c329bf473bba844d5827f069ced7f639dcdd25
omarzouaoui <zouaouiomar80@gmail.com>
2022-11-22 18:52:58 +0100

    working on main

--- quiz_app/client/src/components/Main.js
+++ quiz_app/client/src/components/Main.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div className='container'>
+        <h1 className='title text-light'>Quiz App</h1>
+
+        <ol>
+            <li>You will be asked 10 questions one after another.</li>
+        </ol>
+    </div>
   )
 }
