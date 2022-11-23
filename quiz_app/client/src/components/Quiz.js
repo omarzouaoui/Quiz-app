b81480099ca107cb8a5e360f9f41a59ebfcc399c
omarzouaoui <zouaouiomar80@gmail.com>
2022-11-23 14:21:37 +0100

    questions

--- quiz_app/client/src/components/Quiz.js
+++ quiz_app/client/src/components/Quiz.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import "./Questions";
 
 export default function Quiz() {
+
+  /** next button e handler*/
+  function onNext(){
+    console.log("on Next");
+  };
+
+  /** prev button e handler*/
+  function onPrev(){
+    console.log("on Prev");
+  };
+
   return (
     <div className='container'>
       <h1 className='title text-light'>Quiz Application</h1>
 
       {/** display questions*/}
       <div className='grid'>
-        <button>Prev</button>
-        <button>Next</button>
+        <button className='btn prev' onClick={onPrev}>Prev</button>
+        <button className='btn next' onClick={onNext}>Next</button>
       </div>
 
     </div>
