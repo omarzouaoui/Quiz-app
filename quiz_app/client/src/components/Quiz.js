342356809069e89094b95648c9c6ff37037bb954
omarzouaoui <zouaouiomar80@gmail.com>
2022-11-23 14:15:01 +0100

    working on Quiz

--- quiz_app/client/src/components/Quiz.js
+++ quiz_app/client/src/components/Quiz.js
@@ -4,6 +4,13 @@ export default function Quiz() {
   return (
     <div className='container'>
       <h1 className='title text-light'>Quiz Application</h1>
+
+      {/** display questions*/}
+      <div className='grid'>
+        <button>Prev</button>
+        <button>Next</button>
+      </div>
+
     </div>
   )
 }
