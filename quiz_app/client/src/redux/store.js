e3b3858eb306818e680da7df235329601a58c59b
omarzouaoui <zouaouiomar80@gmail.com>
2022-11-25 19:10:09 +0100

    Reducers done

--- quiz_app/client/src/redux/store.js
+++ quiz_app/client/src/redux/store.js
@@ -2,11 +2,11 @@ import {combineReducers, configureStore} from '@reduxjs/toolkit';
 
 /**Call */
 import questionReducer from './question_reducer';
-import { resultReducer } from './result_reducer';
+import resultReducer from './result_reducer';
 
 const rootReducer = combineReducers({
     questions : questionReducer,
-    // result : resultReducer
+    result : resultReducer
 })
 
 /**Create store*/
