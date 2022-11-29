8090eb79933aaa79e84c98666a48bbe3a46c913f
omarzouaoui <zouaouiomar80@gmail.com>
2022-11-29 14:22:52 +0100

    listening

--- quiz_app/server/server.js
+++ quiz_app/server/server.js
@@ -1,6 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { config } from "dotenv";
 
 const app = express();
+
+/**middlewares*/
+app.use(morgan('Page resfreshed'));
+app.use(cors());
+app.use(express.json());
+config();
+
+/**App PORT*/
+const port = process.env.PORT || 8080;
 
 /**routes*/
 app.get("/", (req, res) => {
@@ -10,3 +22,7 @@ app.get("/", (req, res) => {
         res.json(err);
     }
 })
+
+app.listen(port, () => {
+    console.log(`Server Running on http://localhost:${port}`);    
+} )
